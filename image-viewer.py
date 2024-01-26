c65f02b1e052bdc996a8a02fa7bcad35f64c7d33
pokabu55 <okazumaru@gmail.com>
2024-01-26 23:40:42 +0900

    修正中

--- image-viewer.py
+++ image-viewer.py
@@ -1,7 +1,20 @@
 import flet as ft
 
 
+def on_dialog_result(e: ft.FilePickerResultEvent):
+    print("Selected files:", e.files)
+    print("Selected file or directory:", e.path)
+
+
 def main(page: ft.Page):
+    # 全体レイアウト
+    page.window_max_width = 640
+    page.window_max_height = 480
+    page.title = "Image Viewer"
+
+    file_picker = ft.FilePicker(on_result=on_dialog_result)
+    print(file_picker)
+
     pass
 
 
